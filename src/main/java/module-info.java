3611dc8c4a895690cae722486c30f33b3e22f314
salmanmahmudsakib <smsakib.official@gmail.com>
2025-08-04 23:14:05 +0600

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module groupfortyone.group41_starmaker {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens groupfortyone.group41_starmaker to javafx.fxml;
+    exports groupfortyone.group41_starmaker;
+}
