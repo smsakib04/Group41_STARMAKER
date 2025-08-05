a65cd925aac4e4c9eedb0fd2e8860355f526eeef
Raghib-2003 <raghibishraq80@gmail.com>
2025-08-05 20:41:31 +0600

    singer profile, upload a song commit

---
+++ src/main/java/groupfortyone/group41_starmaker/Raghib/ManageSingerProfileController.java
@@ -0,0 +1,35 @@
+package groupfortyone.group41_starmaker.Raghib;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ManageSingerProfileController
+{
+    @javafx.fxml.FXML
+    private Label confirmationmessagelabel;
+    @javafx.fxml.FXML
+    private TextField genretextfield;
+    @javafx.fxml.FXML
+    private Label confirmationlabel;
+    @javafx.fxml.FXML
+    private TextField nametextfield;
+    @javafx.fxml.FXML
+    private TextField biotextfield;
+    @javafx.fxml.FXML
+    private TextField emailtextfield;
+    @javafx.fxml.FXML
+    private TextField agetextfield;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void gobackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void savechangesOnAction(ActionEvent actionEvent) {
+    }
+}
