dd35883d7ba202306f6b3f409e3205d2ceafa67d
salmanmahmudsakib <smsakib.official@gmail.com>
2025-08-05 14:05:59 +0600

    Added CSO Dashboard

--- src/main/java/groupfortyone/group41_starmaker/Salman/CEODashboardController.java
+++ src/main/java/groupfortyone/group41_starmaker/Salman/CEODashboardController.java
@@ -1,7 +1,60 @@
 package groupfortyone.group41_starmaker.Salman;
 
+import groupfortyone.group41_starmaker.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class CEODashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void monthlyIncomeReport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logout(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setScene(nextScene);
+            nextStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleMeeting(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void recruitmentRequisition(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewAllEmployees(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendGlobalMessage(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewUserFeedback(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void totalAppUser(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignTasks(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/groupfortyone/group41_starmaker/Salman/CSODashboardController.java
+++ src/main/java/groupfortyone/group41_starmaker/Salman/CSODashboardController.java
@@ -1,7 +1,60 @@
 package groupfortyone.group41_starmaker.Salman;
 
+import groupfortyone.group41_starmaker.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class CSODashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logout(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setScene(nextScene);
+            nextStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void markQueryAsPriority(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void replyToQuery(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchQuery(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewSolvedQueries(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteASolvedQuery(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void markQueryAsSolved(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateFAQ(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void readQuery(ActionEvent actionEvent) {
+    }
+}
