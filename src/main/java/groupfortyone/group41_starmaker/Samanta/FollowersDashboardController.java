7e8a1f6c69a0cd61b06d341773db125808f4f273
Nyanmochii <samantaahmed004@gmail.com>
2025-08-06 14:02:10 +0600

    Follower scene switch

--- src/main/java/groupfortyone/group41_starmaker/Samanta/FollowersDashboardController.java
+++ src/main/java/groupfortyone/group41_starmaker/Samanta/FollowersDashboardController.java
@@ -33,6 +33,16 @@ public class FollowersDashboardController
 
     @javafx.fxml.FXML
     public void messageFollowerOA(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Samanta/MessageFollowerDashboard.fxml"));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            nextStage.setScene(nextScene);
+            nextStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @javafx.fxml.FXML
