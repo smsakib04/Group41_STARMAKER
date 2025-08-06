d1ebd12db2b1e5b868d8aaad77a262b2cb71835a
Nyanmochii <samantaahmed004@gmail.com>
2025-08-06 14:25:30 +0600

    Viewer FXML SS

---
+++ src/main/java/groupfortyone/group41_starmaker/Samanta/SongSearchDashboardController.java
@@ -0,0 +1,29 @@
+package groupfortyone.group41_starmaker.Samanta;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class SongSearchDashboardController
+{
+    @javafx.fxml.FXML
+    private TableColumn genrecolumn;
+    @javafx.fxml.FXML
+    private TextField nameofthesongtextfield;
+    @javafx.fxml.FXML
+    private TableColumn descriptioncolumn;
+    @javafx.fxml.FXML
+    private TableColumn songtitlecolumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousOA(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/groupfortyone/group41_starmaker/Samanta/ViewerDashboardController.java
+++ src/main/java/groupfortyone/group41_starmaker/Samanta/ViewerDashboardController.java
@@ -1,7 +1,18 @@
 package groupfortyone.group41_starmaker.Samanta;
 
+import javafx.event.ActionEvent;
+
 public class ViewerDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void searchSingerOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchSongOA(ActionEvent actionEvent) {
+    }
+}
