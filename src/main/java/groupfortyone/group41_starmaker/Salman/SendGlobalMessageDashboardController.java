e784de1ab852302c5cb31c25de2e5ccd10bbe8ba
salmanmahmudsakib <smsakib.official@gmail.com>
2025-08-06 11:42:57 +0600

    Scene Change

--- src/main/java/groupfortyone/group41_starmaker/Salman/SendGlobalMessageDashboardController.java
+++ src/main/java/groupfortyone/group41_starmaker/Salman/SendGlobalMessageDashboardController.java
@@ -1,8 +1,13 @@
 package groupfortyone.group41_starmaker.Salman;
 
+import groupfortyone.group41_starmaker.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class SendGlobalMessageDashboardController
 {
@@ -17,6 +22,15 @@ public class SendGlobalMessageDashboardController
 
     @javafx.fxml.FXML
     public void previousOA(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Salman/CEO_Dashboard.fxml"));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setScene(nextScene);
+            nextStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @javafx.fxml.FXML
--- src/main/java/groupfortyone/group41_starmaker/Salman/TotalAppUserDashboardController.java
+++ src/main/java/groupfortyone/group41_starmaker/Salman/TotalAppUserDashboardController.java
@@ -1,7 +1,12 @@
 package groupfortyone.group41_starmaker.Salman;
 
+import groupfortyone.group41_starmaker.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class TotalAppUserDashboardController
 {
@@ -19,5 +24,14 @@ public class TotalAppUserDashboardController
 
     @javafx.fxml.FXML
     public void previousOA(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Salman/CEO_Dashboard.fxml"));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setScene(nextScene);
+            nextStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
