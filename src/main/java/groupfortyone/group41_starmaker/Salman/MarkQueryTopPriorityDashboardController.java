9ae15a66e70f5d702ecd61d0c90b7dd339aaff81
salmanmahmudsakib <smsakib.official@gmail.com>
2025-08-06 23:20:29 +0600

    All Scene Done

---
+++ src/main/java/groupfortyone/group41_starmaker/Salman/MarkQueryTopPriorityDashboardController.java
@@ -0,0 +1,29 @@
+package groupfortyone.group41_starmaker.Salman;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MarkQueryTopPriorityDashboardController
+{
+    @javafx.fxml.FXML
+    private TableColumn queryNoCol;
+    @javafx.fxml.FXML
+    private TableColumn nameCol;
+    @javafx.fxml.FXML
+    private TableView markTopPriorityTV;
+    @javafx.fxml.FXML
+    private TableColumn queryCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void markAsPriorityOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/groupfortyone/group41_starmaker/Salman/SearchQueryDashboardController.java
@@ -0,0 +1,36 @@
+package groupfortyone.group41_starmaker.Salman;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class SearchQueryDashboardController
+{
+    @javafx.fxml.FXML
+    private TableColumn queryNoCol;
+    @javafx.fxml.FXML
+    private TextField searchQueryTF;
+    @javafx.fxml.FXML
+    private TableColumn nameCol;
+    @javafx.fxml.FXML
+    private TableView searchTV;
+    @javafx.fxml.FXML
+    private TableColumn queryCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void viewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchOA(ActionEvent actionEvent) {
+    }
+}
