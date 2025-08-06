dca6fa883936229153d9dea78bdee115985361ca
salmanmahmudsakib <smsakib.official@gmail.com>
2025-08-06 23:46:58 +0600

    FXML & Buttons

---
+++ src/main/java/groupfortyone/group41_starmaker/Salman/UpdateFAQDashboardController.java
@@ -0,0 +1,25 @@
+package groupfortyone.group41_starmaker.Salman;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class UpdateFAQDashboardController
+{
+    @javafx.fxml.FXML
+    private TextArea updateFAQTA;
+    @javafx.fxml.FXML
+    private Label updateFAQLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updateFAQOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousOA(ActionEvent actionEvent) {
+    }
+}
