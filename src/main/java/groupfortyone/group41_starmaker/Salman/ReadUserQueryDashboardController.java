62dd22e150f9c8c00d3090d81cd3a8fd137d4b5a
salmanmahmudsakib <smsakib.official@gmail.com>
2025-08-06 22:03:16 +0600

    FXML & Class Crte

---
+++ src/main/java/groupfortyone/group41_starmaker/Salman/ReadUserQueryDashboardController.java
@@ -0,0 +1,31 @@
+package groupfortyone.group41_starmaker.Salman;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.TableColumn;
+
+public class ReadUserQueryDashboardController
+{
+    @javafx.fxml.FXML
+    private TableColumn queryNoCol;
+    @javafx.fxml.FXML
+    private TableColumn nameCol;
+    @javafx.fxml.FXML
+    private TableColumn QueryCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void userQueryTV(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/groupfortyone/group41_starmaker/Salman/ReplyToQueryDashboardController.java
@@ -0,0 +1,35 @@
+package groupfortyone.group41_starmaker.Salman;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
+public class ReplyToQueryDashboardController
+{
+    @javafx.fxml.FXML
+    private TableColumn queryNoCol;
+    @javafx.fxml.FXML
+    private TableColumn queryNameCol;
+    @javafx.fxml.FXML
+    private TableView replyQueryTV;
+    @javafx.fxml.FXML
+    private Label confirmationMessageLabel;
+    @javafx.fxml.FXML
+    private TextArea replyTA;
+    @javafx.fxml.FXML
+    private TableColumn queryCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void previousOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendOA(ActionEvent actionEvent) {
+    }
+}
