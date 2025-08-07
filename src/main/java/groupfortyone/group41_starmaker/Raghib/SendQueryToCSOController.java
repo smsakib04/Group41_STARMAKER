11b71282e0bb6e0161228344f81787687144349a
Raghib-2003 <raghibishraq80@gmail.com>
2025-08-07 22:20:42 +0600

    Send Query To CSO

---
+++ src/main/java/groupfortyone/group41_starmaker/Raghib/SendQueryToCSOController.java
@@ -0,0 +1,28 @@
+package groupfortyone.group41_starmaker.Raghib;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SendQueryToCSOController
+{
+    @javafx.fxml.FXML
+    private AnchorPane querytextarea;
+    @javafx.fxml.FXML
+    private Label confirmationlabel;
+    @javafx.fxml.FXML
+    private TextField usernametextfield;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void gobackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+}
