e9c85136e74933d79181a54f29be2e3609a11782
Raghib-2003 <raghibishraq80@gmail.com>
2025-08-16 13:18:43 +0600

    File handling class opened

--- src/main/java/groupfortyone/group41_starmaker/Raghib/AppendableObjectOutputStreamCampaign.java
+++ src/main/java/groupfortyone/group41_starmaker/Raghib/AppendableObjectOutputStreamCampaign.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
-public class AppendableObjectOutputStream extends ObjectOutputStream {
-    public AppendableObjectOutputStream(OutputStream out) throws IOException {
+public class AppendableObjectOutputStreamCampaign extends ObjectOutputStream {
+    public AppendableObjectOutputStreamCampaign(OutputStream out) throws IOException {
         super(out);
     }
 
@@ -13,4 +13,3 @@ public class AppendableObjectOutputStream extends ObjectOutputStream {
     protected void writeStreamHeader() throws IOException {
     }
 }
-
